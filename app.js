177976c9e9178849a9febe37ab3d4e210193fdb1
Ajay Verma <tigersis1995@gmail.com>
2024-12-11 01:05:21 +0530

    Creating the node server_3.27

---
+++ app.js
@@ -0,0 +1,29 @@
+//absolute path -- > require('./http');
+const http = require("http");
+
+
+
+// function rqListner(req,res){
+
+// }
+//we can create a functions and pass into the createServer as a callback.
+
+
+//2nd method
+// http.createServer(function(req,res){
+
+// });
+
+
+//3rd method
+//server variable stores the instance of HTTP Servers using node.js, which have certain properties and the meyhods.
+
+// for eg..(Event Emitter Behavior)--> request,connection,close,error
+//Important Methods-->server.listen(port,callback);
+
+const server = http.createServer((req,res)=>{
+    console.log(req);
+});
+
+server.listen(3000);
+
